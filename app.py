6a0cee449b673eddfe153c2ec012e37c3f39b2ff
SoniBoy30 <110685372+Sonyboy30@users.noreply.github.com>
2026-08-14 18:47:09 -0400

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import streamlit as st
 import yfinance as yf
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt 
 
 # ----------------------------- Helper Functions -----------------------------
 
